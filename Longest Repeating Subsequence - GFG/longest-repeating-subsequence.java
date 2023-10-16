75be1f0a750ced5a1ae0345ae08286cdf8bac367
Afsara Tasnim <tasnimafsara12@gmail.com>
2023-10-16 23:12:27 +0600

    Added solution - LeetHub

---
+++ Longest Repeating Subsequence - GFG/longest-repeating-subsequence.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String str = br.readLine().trim();
+            Solution ob = new Solution();
+            int ans = ob.LongestRepeatingSubsequence(str);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public static int lcs(String s1, String s2, int n, int m){
+        int dp[][] = new int[n+1][m+1];
+        for(int i=0;i<dp.length;i++){
+            dp[i][0] = 0;
+        }
+        for(int i=0;i<dp[0].length;i++){
+            dp[0][i] = 0;
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1) && i!=j){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    int ans1 = dp[i][j-1];
+                    int ans2 = dp[i-1][j];
+                    dp[i][j]=  Math.max(ans1, ans2);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+    public int LongestRepeatingSubsequence(String str)
+    {
+        // code here
+        return lcs(str,str,str.length(),str.length());
+        
+    }
+}
